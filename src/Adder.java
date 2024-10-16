36542ac7b9cd6874342125cd0a2331f53c17cec7
Abd-Elhakim-Arabet <abd.elhakim.arabet@ensia.edu.dz>
2024-10-16 16:18:30 +0100

    initial commit

---
+++ src/Adder.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Adder extends Calculator{
+    @Override
+    public double compute(double A, double B) {
+        return A + B;
+    }
+}
---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+package src;
+
+/**
+ * Abstract Class 
+ */
+public abstract class Calculator {
+    /**
+     * Abstract method that will be implemented by subclasses 
+     * */ 
+    public abstract double compute(double A, double B);
+}
